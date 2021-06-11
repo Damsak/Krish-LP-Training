378189ee36046b6703387c064b2af69e8b4df459
Damsak <damsakaqua@gmail.com>
2021-06-11 10:49:35 +0530

    Microservices Practicals update

---
+++ MicroservicesPracticals/ConfigurationServers/LocalConfig/LocalconfigRent/src/test/java/com/damsak/rentcloud/configlocal/ConfigLocalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damsak.rentcloud.configlocal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigLocalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
